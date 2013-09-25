ca7bce8793e72f391b35ecf632a4c212bc19fb00
Inbound Now <david@inboundnow.com>
2013-09-24 19:56:37 -0700

    updates

--- debug-plugin-activation-errors.php
+++ debug-plugin-activation-errors.php
@@ -9,33 +9,49 @@ Author URI: http://www.inboudnow.com/
 */
 
 if (is_admin()) {
-	
+    
 /* Debug Plugin Activation errors */
-add_action('activated_plugin','save_error');
-function save_error(){
-    update_option('plugin_error',  ob_get_contents());
+add_action('activated_plugin','plugin_activation_save_error');
+function plugin_activation_save_error(){
+    update_option('plugin_activation_error_message',  ob_get_contents());
 }
 
+/* Display Admin Notices */
 add_action('admin_notices', 'plugin_activation_error_notice');
 function plugin_activation_error_notice() {
-    global $current_user ;
-    $error = get_option('plugin_error');
-   	if ($error === ""){
-   		$error = 'Error Log Empty. Run your plugin activation';
-   	} 
-        echo '<div class="updated"><h2>The Issues Causing "unexpected output" are:</h2><p>';
+
+        $error = get_option('plugin_activation_error_message');
+        if ($error !== ""){
+        echo '<div class="updated"><h2>The Issues Causing "unexpected output" are:</h2>';
         echo "<pre>";
         echo $error;
         echo "</pre>";
         echo "<h2><a href='?plugin_activation_message_reset=1'>Clear Warnings and Try Plugin Install Again</a></h2>";
-        echo "</p></div>";
+        echo "</div>";
+        } else {
+        echo '<div class="updated"><h2>Run Your Plugin Activation to Show Error Log</h2></div>';
+        }
 }
+
+/* Reset Error Message */
 add_action('admin_init', 'plugin_activation_message_reset');
 function plugin_activation_message_reset() {
     global $current_user;
         if ( isset($_GET['plugin_activation_message_reset']) && '1' == $_GET['plugin_activation_message_reset'] ) {
-         update_option('plugin_error',  'Activation Error Cleared. Run your plugin activation again from plugins admin page. Turn off this plugin to get rid of these messages'); // clear errors
+         update_option('plugin_activation_error_message',  'Activation Error Cleared. Run your plugin activation again from plugins admin page. Turn off this plugin to get rid of these messages'); // clear errors
     }
+}
+
+/* Activation Method  */
+register_activation_hook( __FILE__, 'plugin_activation_error_setup' );
+function plugin_activation_error_setup() {
+    add_option('plugin_activation_error_message',  'Error Log Empty. Run your plugin activation');
+}
+
+/* Delete on deactivation */
+register_deactivation_hook( __FILE__, 'plugin_activation_error_delete' );
+function plugin_activation_error_delete() {
+    delete_option( 'plugin_activation_error_message' );
 }
 
 }
